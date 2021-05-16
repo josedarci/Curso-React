7e05f1e94a410e9034384225ff48c7e5f4069da1
José Darci Rodrigues Junior <juniorinternet@gmail.com>
2021-05-16 18:38:35 -0300

    Atualizando

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,76 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  //constructor(props) {
-    //super(props);
-    //this.handlePClick = this.handlePClick.bind(this); para nao fazer isso usar rrow function nos metodos
-    /* this.state = {
-      name: 'Josér Daci',
-      counter: 0
-    }; */
     state = {
-      name: 'Josér Daci',
-      counter: 0
+     posts: [
+       {
+         id: 1,
+         title: 'O titulo 1',
+         body: 'O corpo 1'
+       },
+       {
+        id: 2,
+        title: 'O titulo 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O titulo 3',
+        body: 'O corpo 3'
+      }
+     ]
     };
-  //}
-  
-  handlePClick = () => {
-    this.setState({name: 'Junior'})
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const {counter  } = this.state;
-    
-    this.setState({counter: counter + 1, name: 'Junior'})
-  }
-
 
   render() {
-    const {name, counter} = this.state;
+    const {posts} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} | {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o Link!
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          </div>
+        )
+        )}
       </div> 
     );
   }
 }
-/* function App() {
-  return (
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá Mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> 
-  );
-} */
 
 export default App;
